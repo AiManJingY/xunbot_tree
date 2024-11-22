4b495d41600e7fb9c9e91c45b8f11007035bacd7
AiManJingY <3114998754@qq.com>
2024-11-22 19:15:56 +0800

    Update Android.mk

--- Android.mk
+++ Android.mk
@@ -10,3 +10,6 @@ LOCAL_PATH := $(call my-dir)
 ifeq ($(TARGET_DEVICE),xunbot)
 include $(call all-subdir-makefiles,$(LOCAL_PATH))
 endif
+ifneq ($(BOARD_RECOVERY_BLDRMSG_OFFSET),)
+    LOCAL_CFLAGS += -DBOARD_RECOVERY_BLDRMSG_OFFSET=$(BOARD_RECOVERY_BLDRMSG_OFFSET)
+endif
